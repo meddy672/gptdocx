482d7eed8460b44a4c4606f3c45dae0de4b111e2
Matthew Eddy <meddy672@gmail.com>
2024-01-28 15:43:27 -0500

    Automated commit message

---
+++ test/Mock/responseFormats.ts
@@ -0,0 +1,74 @@
+const responseFormats = {
+    basicExample: {
+        title: "",
+        author: "",
+        date: "",
+        content: [
+            {
+                heading: "",
+                prargraph: ""
+            }
+        ]
+    },
+    jobApp: {
+        title: "",
+        jobName: "",
+        overview: "",
+        content: [
+            {
+                heading: "",
+                whatWeOffer: []
+            },
+            {
+                heading: "",
+                responsibilities: []
+            },
+            {
+                heading: "",
+                qulifications: []
+            },
+            {
+                heading: "",
+                salary: ""
+            }
+        ]
+    },
+    coverLetter: {
+        title: "",
+        subTitle: "",
+        email: "",
+        phoneNumber: "",
+        date: "",
+        hiringManger: "",
+        company: "",
+        companyAddress: "",
+        content: [
+            {
+                heading: "",
+                introduction: ""
+            },
+            {
+                heading: "",
+                experience: ""
+            },
+            {
+                conclusion: ""
+            }
+        ]
+    },
+    updateNotice: {
+        title: "",
+        notice: "",
+        overview: "",
+        content: [
+            {
+                heading: "",
+                actionsRequired: []
+            },
+            {
+                heading: "",
+                assistance: ""
+            }
+        ],
+    }
+}
