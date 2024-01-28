8fbcee8d1ffa19f08edbb0945f0af51ef5b72b09
Matthew Eddy <meddy672@gmail.com>
2024-01-28 16:08:51 -0500

    Automated commit message

--- test/GPTDocx.test.ts
+++ test/GPTDocx.test.ts
@@ -16,14 +16,40 @@ describe("GPTDocx", () => {
         docTemplaterSpy.mockRestore();
     })
 
-    test('should return filePath with .docx extension', () => { 
-        const filePath: any = new GPTDocx({
+    test.only('should return filePath with .docx extension', async () => { 
+        const filePath: any = await new GPTDocx({
             format: "basicExample",
             prompt: "Write a paper about Whales."
-        });
+        }).createFile()
         const { name, ext } = path.parse(filePath);
         expect(name).toEqual("HowToTypeFaster");
         expect(ext).toEqual(".docx")
      });
-     
+
+     test('should throw Parse error is undefined', async () => { 
+        let response: any;
+        try {
+             await new GPTDocx({
+                format: "",
+                prompt: "Write a paper about Whales."
+            }).createFile()
+        } catch (error) {
+            response = error;
+        }
+        expect(response).toEqual("Service is not valid. PARSE_SERVICE_REQUEST_ERROR");
+     });
+
+     test('should throw invalid prompt error when prompt is undefined', async () => { 
+        let response: any;
+        try {
+             await new GPTDocx({
+                format: "basicExample",
+                prompt: ""
+            }).createFile()
+        } catch (error) {
+            response = error;
+        }
+        expect(response).toEqual("Error: INVALID_PROMPT");
+     });
+
 })
