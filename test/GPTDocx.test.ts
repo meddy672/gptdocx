bb11aa6e8ebd11545b7435db89f11b630129fdc1
Matthew Eddy <meddy672@gmail.com>
2024-01-28 16:03:00 -0500

    Automated commit message

--- test/GPTDocx.test.ts
+++ test/GPTDocx.test.ts
@@ -1,12 +1,29 @@
+import GPTDocx from "GPTDocx";
 import ChatGPT from "../src/ChatGPT";
 import DocxTemplater from "../src/DocxTemplater";
+import path from 'path'
 
 describe("GPTDocx", () => {
+    let chatGptSpy: any;
+    let docTemplaterSpy: any;
     beforeEach(() => {
-
+        chatGptSpy =  jest.spyOn(ChatGPT.prototype, 'send')
+        docTemplaterSpy = jest.spyOn(DocxTemplater.prototype, 'create');
     });
 
     afterEach(()  => {
-        
+        chatGptSpy.mockRestore();
+        docTemplaterSpy.mockRestore();
     })
+
+    test('should return filePath with .docx extension', () => { 
+        const filePath: any = new GPTDocx({
+            format: "basicExample",
+            prompt: "Write a paper about Whales."
+        });
+        const { name, ext } = path.parse(filePath);
+        expect(name).toEqual("HowToTypeFaster");
+        expect(ext).toEqual(".docx")
+     });
+     
 })
