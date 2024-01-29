73bb23c61925a03152da22126b15e232b5fc1d4e
Matthew Eddy <meddy672@gmail.com>
2024-01-28 23:44:17 -0500

    Automated commit message

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,2 +1,19 @@
 const gptdocx = require('../src');
-console.log(gptdocx)
+
+describe('gptdocx', () => { 
+    test('should include GPTDocx', () => { 
+        expect(gptdocx.GPTDocx).toBeDefined()
+     })
+     test('should include BASIC', () => { 
+        expect(gptdocx.BASIC).toBeDefined()
+     })
+     test('should include COVER_LETTER', () => { 
+        expect(gptdocx.COVER_LETTER).toBeDefined()
+     })
+     test('should include JOB_APP', () => { 
+        expect(gptdocx.JOB_APP).toBeDefined()
+     })
+     test('should include UPDATE_NOTICE', () => { 
+        expect(gptdocx.UPDATE_NOTICE).toBeDefined()
+     })
+ })
