7224c9a3d61e43f917d86a763b395d3b9519626c
Matthew Eddy <meddy672@gmail.com>
2024-01-28 23:55:39 -0500

    Automated commit message

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,9 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
 
 module.exports = {
   target: 'node',
   entry: './src/index.ts', // Specify the entry point
   mode: 'development',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
   resolve: {
     extensions: ['.ts', '.js']
   },
