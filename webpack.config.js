ebc41bf71911d9388db91a2a10c2a2a061046793
Matthew Eddy <meddy672@gmail.com>
2024-01-28 22:23:08 -0500

    Automated commit message

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
       patterns: [
         {
           from: 'src/docx',
-          to: 'dist/docx',
+          to: './docx',
           globOptions: {
             ignore: ['*.ts'],
           },
